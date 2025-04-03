1e32200a01999bd8b14b5741b9a89b02acab1349
cr3wcr3w <racelisjk@gmail.com>
2025-04-03 13:56:50 +0800

    refactor: use React instead of Solid

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name: name() }));
+    setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
     <main >
-      <h1>Welcome to Tauri + Solid</h1>
+      <h1>Welcome to Tauri + React</h1>
 
-      <div >
+      <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg"alt="Vite logo" />
+          <img src="/vite.svg" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank">
           <img src="/tauri.svg" alt="Tauri logo" />
         </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} alt="Solid logo" />
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} alt="React logo" />
         </a>
       </div>
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
         onSubmit={(e) => {
@@ -41,8 +41,8 @@ function App() {
         />
         <button type="submit">Greet</button>
       </form>
-      <p>{greetMsg()}</p>
-      <p class="bg-red-900">tailwind</p>
+      <p>{greetMsg}</p>
+      <p className="bg-red-900">tailwind</p>
     </main>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 // @ts-expect-error process is a nodejs global
@@ -7,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [solid(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
